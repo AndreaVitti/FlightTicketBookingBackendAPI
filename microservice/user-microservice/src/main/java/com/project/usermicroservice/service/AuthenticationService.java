f438e03a15370ed6c14b53a9dc56c7c2bd0f66c8
AndreaVitti <av.andreavitti@gmail.com>
2025-05-22 00:52:32 +0200

    Added refresh and logout functions

--- microservice/user-microservice/src/main/java/com/project/usermicroservice/service/AuthenticationService.java
+++ microservice/user-microservice/src/main/java/com/project/usermicroservice/service/AuthenticationService.java
@@ -5,10 +5,17 @@ import com.project.usermicroservice.DTO.RegisterRequest;
 import com.project.usermicroservice.DTO.Response;
 import com.project.usermicroservice.authConfig.JwtService;
 import com.project.usermicroservice.entity.User;
+import com.project.usermicroservice.exception.UserNotFound;
 import com.project.usermicroservice.repository.UserRepository;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +27,9 @@ public class AuthenticationService {
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
+    @Value("${expire.refresh-token}")
+    private int refreshTokenExpire;
+
 
     public Response register(RegisterRequest regRequest) {
         User user = new User();
@@ -40,24 +50,43 @@ public class AuthenticationService {
     }
 
     public Response login(AuthRequest authRequest) {
+        Response response = new Response();
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
                 authRequest.email(),
                 authRequest.password())
         );
-        User user = userRepository.findByEmail(authRequest.email()).orElseThrow();
+        User user = userRepository.findByEmail(authRequest.email()).orElseThrow(() -> new UserNotFound("User not found"));
+        generateAccessAndRefreshTokens(user, response);
+        return response;
+    }
 
-        /*Generate both refresh and access tokens*/
-        String jwt = jwtService.generateToken(user, user.getId());
-
+    public Response refresh(String refreshToken) {
         Response response = new Response();
-        response.setHttpCode(200);
-        response.setToken(jwt);
-        response.setUserId(user.getId());
+        Long userId = Long.parseLong(jwtService.extractSubject(refreshToken));
+        User user = userRepository.findById(userId).orElseThrow(() -> new UserNotFound("User not found"));
+        generateAccessAndRefreshTokens(user, response);
         return response;
     }
 
     public Boolean verifyUser(String email) {
         User user = userRepository.findByEmail(email).orElse(null);
         return user != null;
+    }
+
+    public void logout(HttpServletRequest request, HttpServletResponse response) {
+        SecurityContextHolder.clearContext();
+        Cookie cookie = new Cookie("Refresh-cookie", "");
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+        response.setStatus(HttpStatus.OK.value());
+    }
+
+    private void generateAccessAndRefreshTokens(User user, Response response) {
+        String accessToken = jwtService.generateAccessToken(user, user.getId());
+        String refreshToken = jwtService.generateRefreshToken(user.getId());
+        response.setHttpCode(200);
+        response.setAccessToken(accessToken);
+        response.setRefreshToken(refreshToken);
+        response.setUserId(user.getId());
     }
 }
