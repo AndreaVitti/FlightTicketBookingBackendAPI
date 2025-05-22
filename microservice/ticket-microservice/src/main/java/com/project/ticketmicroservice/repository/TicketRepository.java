062b4183c3dbb2e06e0bd4a9bdbca39ef8d399ad
AndreaVitti <av.andreavitti@gmail.com>
2025-05-22 17:26:33 +0200

    Added pagination

--- microservice/ticket-microservice/src/main/java/com/project/ticketmicroservice/repository/TicketRepository.java
+++ microservice/ticket-microservice/src/main/java/com/project/ticketmicroservice/repository/TicketRepository.java
@@ -1,6 +1,8 @@
 package com.project.ticketmicroservice.repository;
 
 import com.project.ticketmicroservice.entity.Ticket;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,7 +11,7 @@ import java.util.Optional;
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
     Optional<Ticket> findByConfirmCode(String confirmCode);
 
-    List<Ticket> findByUserId(Long userId);
+    Optional<Page<Ticket>> findByUserId(Long userId, Pageable pageable);
 
     void deleteByConfirmCode(String confirmCode);
 }
